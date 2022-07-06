c48a8770c4d27ed7a0e7024bd4f4b5a53ff0a8b3
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-05 23:45:55 -0700

    commit tree e73e9efc76b5cfc70be4793d85c8d7250d3e7cc2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, whatItIs){
+   const thanks = [] 
+    for (let i = 0; i < names.length; i++){
+        thanks.push (`Thank you, ${names[i]}, for the wonderful ${whatItIs} gift!`)
+    }
+    return thanks;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], 'suprise')
+
+function countDown(){
+    let down = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (var i = down.length - 1; i >= 0; i--) {
+    console.log(down[i]);
+    }
+}
